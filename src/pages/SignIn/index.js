c3e205699586db9bf2f9a0da85c24a7cebc5f502
Marcio Curvello <marciocurvello@gmail.com>
2023-05-01 21:51:59 -0300

    hide passwords

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -48,6 +48,7 @@ export default function SignIn() {
             autoCapitalize="none"
             value={password}
             onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
           />
         </AreaInput>
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -53,6 +53,7 @@ export default function SignUp() {
             autoCapitalize="none"
             value={password}
             onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
           />
         </AreaInput>
 
